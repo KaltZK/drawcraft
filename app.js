071e7a5b38b5ab5dd52fdbeaac0a2f27d7957706
KaltZK <715480954@qq.com>
2015-03-27 21:01:25 +0800

    把界面弄得漂亮一点 可以分房间了

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var io = require('socket.io').listen(8080);
 var express = require('express');
 var url=require('url');
@@ -10,23 +9,38 @@ var app = express();
         app.set('port',process.env.VMC_APP_PORT||3000);
         app.set('views',__dirname+'/views');
         app.set('view engine','ejs');
-        app.set('static',__dirname+'/public');
+        app.use(express.static(__dirname + '/static'));//设置静态文件
 }).call();
 
 app.get('/',function(req,res){
         res.render("index");
 });
 app.get('/app',function(req,res){
-        data = url.parse(req.url,true).query;
-        res.render("app",{username:data["username"]});
+        var data = url.parse(req.url,true).query;
+        res.render("app",{username:data["user"],roomname:data["room"]});
 });
 
 app.listen(8000);
 
 
 io.on("connection",function(socket){
+        socket.join("");
+        socket.on('enter_room',function(data){
+                socket.join(data.room);
+                socket.broadcast.to(data.room).emit("text_message",{
+                        author: "System",
+                        text: data.user + " joined "+data.room,
+                });
+        });
+        socket.on('leave_room',function(data){
+                socket.leave(data.room);
+                socket.broadcast.to(data.room).emit("text_message",{
+                        author: "System",
+                        text: data.user + " left "+data.room,
+                });
+        }); 
         socket.emit("news",{hello: "world"});
-        socket.on("text-message",function(message){
-                socket.broadcast.emit('text-message',message);
+        socket.on("text_message",function(message){
+                socket.broadcast.to(message.room).emit('text_message',message);
         });
 });
