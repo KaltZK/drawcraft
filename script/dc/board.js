ae6573b2f1e226c70bc1e9dff91e879a62ee3114
KaltZK <715480954@qq.com>
2015-06-25 13:49:12 +0800

    加入了未完成的fromStruct和toStruct TODO见下面
    TODO
    * 想办法统一缩放、平移和绘制三个操作的管理，用锁，或者能同时进行更好
    现在这样虽然没什么大问题但怪怪的
    
    * 测试现有的struct转换是否能用
    
    * AbsPos提供关于当前视野大小的信息
    
    * 把涉及polyline的具体操作从board移到graphic
    
    * 添加选中和删除功能
    
    ? 支持更多形状
    
    * shift拉直线功能

--- script/dc/board.js
+++ script/dc/board.js
@@ -6,11 +6,6 @@ function($,svg,Graphic,AbsPos){return function(id){
         var graphics=[];
         var abspos=this.abspos=new AbsPos(this);
 
-        g=draw.polyline("0,0 "+[document.body.clientWidth,document.body.clientHeight].toString());
-        g.stroke({color:"blue",width:2,});
-
-        graphics.push(new Graphic(g,0,this));
-
         $(element).bind("mousewheel",function(evt,delta){
                 abspos.dzoom(delta);
                 graphics.forEach(function(gr){gr.updateZoom()});
@@ -34,8 +29,9 @@ function($,svg,Graphic,AbsPos){return function(id){
                 case 1:
                         var     points=[],
                                 line=draw.polyline([evt.clientX,evt.clientY].toLocaleString());
-                        line.stroke({color:"blue",width:1});
-                        line.fill("none");
+                        var style={stroke:{color:"blue",width:1},fill:"none"};
+                        line.stroke(style.stroke);
+                        line.fill(style.fill);
                         $(element).bind("mousemove",function(evt){
                                 points.push([evt.clientX,evt.clientY]);
                                 line.plot(points);
@@ -44,7 +40,7 @@ function($,svg,Graphic,AbsPos){return function(id){
                                 points.push([evt.clientX,evt.clientY]);
                                 line.plot(points);
                                 if(points.length>1)
-                                        graphics.push(new Graphic(line,abspos.z(),self));
+                                        graphics.push(new Graphic(line,self,style));
                                 $(element).unbind("mousemove");
                                 $(element).unbind("mouseup");
                         });
--- script/dc/graphic.js
+++ script/dc/graphic.js
@@ -2,38 +2,54 @@ define("dc/graphic",['jquery','svg','dc/posfuncs'],function($,svg,posfuncs){
         //debug用
         centerdiv.style.left=posfuncs.centerX();
         centerdiv.style.top=posfuncs.centerY();
-Graphic=function(polyline,zIndex,board){
+Graphic=function(polyline,board,style){
         var self=this;
         this.polyline=polyline;
         this.board=board;
-        var abspos=board.abspos;
+        var abspos=this.abspos=this.board.abspos;
         
         var pwidth=polyline.width(),pheight=polyline.height();
         var px=polyline.x(),py=polyline.y();//图形相对屏幕位置
-        var     width=polyline.width()/posfuncs.zoomMapping(zIndex),
-                height=polyline.height()/posfuncs.zoomMapping(zIndex),
-                absx=abspos.mapXToAbs(px),//图形相对绝对坐标位置
-                absy=abspos.mapYToAbs(py);
+        var     width=polyline.width()/posfuncs.zoomMapping(abspos.z()),
+                height=polyline.height()/posfuncs.zoomMapping(abspos.z());
+        var absx=this.absx=abspos.mapXToAbs(px);//图形相对绝对坐标位置
+        var absy=this.absy=abspos.mapYToAbs(py);
+        this.absPoints=polyline._array.value.map(function(pos){
+                return [abspos.mapXToAbs(pos[0]),
+                abspos.mapXToAbs(pos[1])];
+        });
 
 
         this.updateZoom=function(){
-                var     centerX=window.screen.availWidth/2,
-                        centerY=window.screen.availHeight/2;
+                var     centerX=posfuncs.centerX(),
+                        centerY=posfuncs.centerY();
                 var multiple=posfuncs.zoomMapping(abspos.z());
                 if(multiple<1e-6) return;
-                polyline.size(width*multiple,height*multiple);
-                polyline.move(abspos.reMapXFromAbs(absx),abspos.reMapYFromAbs(absy));
+                this.polyline.size(width*multiple,height*multiple);
+                this.polyline.move(abspos.reMapXFromAbs(this.absx),
+                        abspos.reMapYFromAbs(this.absy));
         };
         this.updatePos=function(){
-                polyline.move(abspos.reMapXFromAbs(absx),abspos.reMapYFromAbs(absy));
+                this.polyline.move(abspos.reMapXFromAbs(this.absx),
+                        abspos.reMapYFromAbs(this.absy));
         };
         
         this.toStruct=function(){
                 return{
                         x:absx,y:absy,
+                        width:width,height:height,
+                        points:this.absPoints,
+                        style:style,
                 };
         };
 };
+Graphic.fromStruct=function(struct,board){
+        var line=board.draw.polyline(
+                struct.points.map(function(pos){
+                        return [abspos.reMapXFromAbs(pos[0]),
+                        abspos.reMapXFromAbs(pos[1])]
+        }));
+        return new this(line,board,struct.style);
+};
 return Graphic;
-
 });
